2c13860c3db9cb7e1301488880ffcdcb7dba15ae
Stephan Vock <stephan.vock@innogames.de>
2013-10-18 22:27:24 +0200

    added a couple of config options (not all of them are implemented yet)

--- script.py
+++ script.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 
-import os
+import sys
+from config import config
 from changelog_builder import VersionControlFactory, Builder
 from changelog_builder.log_parser import LogParserFactory
 
+if len(sys.argv) < 2:
+    raise Exception('Missing argument: Usage python script.py old_branch new_branch')
+
 vcs_factory = VersionControlFactory()
-vcs = vcs_factory.get_vcs('git')
+vcs = vcs_factory.get_vcs(config['vcs'])
 
 log_parser_factory = LogParserFactory()
-log_parser = log_parser_factory.get_parser('git')
+log_parser = log_parser_factory.get_parser(config['vcs'])
 
-output = vcs.get_log(os.path.dirname(os.path.realpath(__file__)), 'test')
+output = vcs.get_log(config['path'], sys.argv[1])
 
 builder = Builder(log_parser)
 builder.set_previous_log(output)
 
-output = vcs.get_log(os.path.dirname(os.path.realpath(__file__)), 'master')
+output = vcs.get_log(config['path'], sys.argv[2])
 
 builder.set_current_log(output)
 
