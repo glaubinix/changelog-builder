e347226dcbaaca081272c23ffc5f1618dacaceee
Stephan Vock <stephan.vock@innogames.de>
2013-10-23 20:31:39 +0200

    fixed svn, this will now return the log for every single commit

--- changelog_builder/version_control/svn.py
+++ changelog_builder/version_control/svn.py
@@ -6,7 +6,7 @@ from changelog_builder.version_control.abstract_vcs import AbstractVcs
 class Svn(object):
 
     def get_log(self, path, branch):
-        pr = subprocess.Popen("svn log " + path + branch, cwd=os.path.dirname(os.path.realpath(__file__)),
+        pr = subprocess.Popen("svn log -g " + path + branch, cwd=os.path.dirname(os.path.realpath(__file__)),
                               shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (out, error) = pr.communicate()
 
